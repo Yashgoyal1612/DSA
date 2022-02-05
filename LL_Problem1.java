7d2ee022d2db129da88961c0da739857e72a66cc
Yash Goyal <80318226+Yashgoyal1612@users.noreply.github.com>
2022-02-05 23:30:49 +0530

    By correction framework
    Make a Linked List & add the following elements to it : (1, 5, 7, 3 , 8, 2, 3).
    //    Search for the number 7 & display its index.

---
+++ LL_Problem1.java
@@ -0,0 +1,15 @@
+package Linked_list;
+import java.util.*;
+public class problem1 {
+  LinkedList<Integer> list = new LinkedList<Integer>();
+        list.addLast(1);
+        list.addLast(5);
+        list.addLast(7);
+        list.addLast(3);
+        list.addLast(8);
+        list.addLast(2);
+        list.addLast(3);
+       System.out.println(list);
+       System.out.println(list.indexOf(7));
+     }
+ }
